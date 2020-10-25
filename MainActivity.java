b6fdf4a39346bd24d9030c8790798fe31c556f27
HasibeGizemSahin <73443318+HasibeGizemSahin@users.noreply.github.com>
2020-10-25 23:14:35 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.odev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity {
+
+    public static void main(String args[]){
+        System.out.println("helloo worldd");
+}
+
+}
